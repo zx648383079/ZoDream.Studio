b056206db4dac2d29fe53ae71bd8b25a6fd56d79
ZoDream <648383079@qq.com>
2018-11-05 23:29:55 +0800

    修改

---
+++ MidiPlayer/MidiPlayer/Midi/Key.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidiPlayer.Midi
+{
+    public class Key
+    {
+        /// <summary>
+        /// 调号 1 - 7
+        /// </summary>
+        public ushort Code { get; set; } = 0;
+
+        /// <summary>
+        /// 0 表示大调，1 表示小调
+        /// </summary>
+        public byte Scale { get; set; }
+
+        /// <summary>
+        /// 音调 -4 - 4
+        /// </summary>
+        public short Beat { get; set; } = 0;
+        /// <summary>
+        /// 速度 .125 - 8  为0时为中止
+        /// </summary>
+        public float Speed { get; set; } = 1;
+
+        public void SetKeyBy12(ushort code)
+        {
+            if (code < 6)
+            {
+                Code = Convert.ToUInt16(code / 2);
+                Scale = Convert.ToByte(code % 2);
+                return;
+            }
+            Code = Convert.ToUInt16(code / 2 + 1);
+            Scale = Convert.ToByte(1 - code % 2);
+        }
+
+        public void SetKeyBy12(int code)
+        {
+            SetKeyBy12(Convert.ToInt16(code));
+        }
+
+        public void SetKeyBy127(int code)
+        {
+            SetKeyBy127(Convert.ToInt16(code));
+        }
+
+        public void SetKeyBy127(ushort code)
+        {
+            SetKeyBy12(code / 12 + 1);
+            Beat = Convert.ToInt16(code % 12 - 4);
+        }
+
+        public Key() { }
+
+        public Key(ushort code)
+        {
+            Code = code;
+        }
+
+        public Key(int code)
+        {
+            Code = Convert.ToUInt16(code);
+        }
+
+        public Key(ushort code, float speed)
+        {
+            Code = code;
+            Speed = speed;
+        }
+
+        public Key(ushort code, byte scale, float speed)
+        {
+            Code = code;
+            Scale = scale;
+            Speed = speed;
+        }
+
+        public Key(ushort code, byte scale, short beat, float speed)
+        {
+            Code = code;
+            Scale = scale;
+            Speed = speed;
+            Beat = beat;
+        }
+    }
+}
---
+++ MidiPlayer/MidiPlayer/Midi/Score.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidiPlayer.Midi
+{
+    /// <summary>
+    /// 简谱
+    /// </summary>
+    public class Score : IEnumerator, IEnumerable
+    {
+        #region 属性
+        /// <summary>
+        /// 曲名
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// 作词
+        /// </summary>
+        public string Author { get; set; }
+        /// <summary>
+        /// 作曲
+        /// </summary>
+        public string Composer { get; set; }
+        /// <summary>
+        /// 演唱
+        /// </summary>
+        public string Singer { get; set; }
+
+        /// <summary>
+        /// 节奏 60 / 每拍
+        /// </summary>
+        public int Tempo { get; set; }
+        /// <summary>
+        /// 调号 1= C
+        /// </summary>
+        public string KeySignature { get; set; }
+        /// <summary>
+        /// 拍号中多少拍为一小节，分子
+        /// </summary>
+        public ushort TimeSignaturePerBar { get; set; }
+        /// <summary>
+        /// 拍号中多少音符为一拍，分母
+        /// </summary>
+        public ushort TimeSignaturePerTime { get; set; }
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public IList<Key> Tracks { get; set; } = new List<Key>();
+        #endregion
+
+        #region 支持循环
+        private int position = -1;
+
+        public object Current
+        {
+            get
+            {
+
+                try
+                {
+                    return Tracks[position];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return ((IEnumerable)Tracks).GetEnumerator();
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return (position < Tracks.Count);
+        }
+
+        public void Reset()
+        {
+            position = 0;
+        }
+        #endregion
+
+
+        /// <summary>
+        /// 设置拍号
+        /// </summary>
+        /// <param name="time">多少拍为一小节</param>
+        /// <param name="note">多少音符为一拍</param>
+        public void SetTimeSignature(int time, int note)
+        {
+            TimeSignaturePerBar = Convert.ToUInt16(time);
+            TimeSignaturePerTime = Convert.ToUInt16(note);
+        }
+
+        public void AppendKey(int code)
+        {
+
+        }
+
+        public void AppendKey12(int code)
+        {
+
+        }
+
+        public void AppendKey127(int code)
+        {
+
+        }
+
+        public void AppendPrevious()
+        {
+
+        }
+
+    }
+}
